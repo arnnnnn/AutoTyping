abf4b1ba9d6049d90db12da2634dcccd35f307b2
YH <dbs8543@gmail.com>
2022-02-14 20:59:43 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace AutoTyping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainTextBOx = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MainTextBOx
+            // 
+            this.MainTextBOx.Location = new System.Drawing.Point(25, 54);
+            this.MainTextBOx.Multiline = true;
+            this.MainTextBOx.Name = "MainTextBOx";
+            this.MainTextBOx.Size = new System.Drawing.Size(552, 596);
+            this.MainTextBOx.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(623, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Page Up을 이용하여 Text Send 기능을 이용가능";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 666);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MainTextBOx);
+            this.Name = "Form1";
+            this.Text = "Auto Typing Software";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MainTextBOx;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+
+
+
+namespace AutoTyping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+        }
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc callback, IntPtr hInstance, uint threadId);
+
+        [DllImport("user32.dll")]
+        static extern bool UnhookWindowsHookEx(IntPtr hInstance);
+        [DllImport("user32.dll")]
+        static extern IntPtr CallNextHookEx(IntPtr idHook, int nCode, int wParam, IntPtr IParam);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr LoadLibrary(string lpFileName);
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode,IntPtr wParam,IntPtr lParam);
+
+        const int WH_KEYBOARD_LL = 13;
+        const int WM_KEYDOWN = 0x100;
+            
+        private LowLevelKeyboardProc _proc = hookProc;
+        private static IntPtr hhook = IntPtr.Zero;
+
+        public void SetHook()
+        {
+            IntPtr hInstace = LoadLibrary("User32");
+            hhook = SetWindowsHookEx(WH_KEYBOARD_LL, _proc, hInstace, 0);
+        }
+        
+        public static void UnHook()
+        {
+            UnhookWindowsHookEx(hhook);
+        }
+
+        public static IntPtr hookProc(int code, IntPtr wParam, IntPtr IParam)
+        {
+            if (code >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(IParam);
+
+                if (vkCode.ToString() == "65")
+                {
+                    //MessageBox.Show("You Pressed a A");
+                }
+                else if (vkCode.ToString() == "33")
+                {
+                    string txt = "Page Up이 눌림";
+                    for (int j = 0; j < txt.Length; j++)
+                    {
+                        string data = txt[j].ToString();
+                        SendKeys.SendWait(data);
+                        Task.Delay(100).Wait();
+
+                    }
+                }
+                return CallNextHookEx(hhook,code,(int)wParam,IParam);
+            }
+            else
+                return CallNextHookEx(hhook, code, (int)wParam, IParam);
+        }
+
+        private void Form1_FormClosing(object sender, CancelEventArgs e)
+        {
+            UnHook();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetHook();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.PageUp:
+                    string txt = MainTextBOx.Text;
+                    for (int i = 0; i < txt.Length; i++)
+                    {
+                        string data = txt[i].ToString();
+                        SendKeys.SendWait(data);
+                        Task.Delay(100).Wait();
+
+                    }
+
+                    break;
+            }
+        }
+
+
+    }   
+}
